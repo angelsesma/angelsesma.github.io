b8264c56b50c05e4bbf7cfdd861d54962c8cf7e5
tiernuki <angelsesma@gmail.com>
2025-07-30 07:39:41 -0600

    Update jsterm1.js

--- data/config/js/jsterm1.js
+++ data/config/js/jsterm1.js
@@ -518,7 +518,7 @@
 
   var term = Object.create(Terminal);
   term.init(CONFIG, "/data/json/sample0.json", COMMANDS, function () {
-    term.enqueue("taogpt").enqueue("raiz").begin();
+    term.enqueue("taogpt").enqueue("hola").enqueue("raiz").begin();
   });
 
   window.typeCommand = function (command) {
